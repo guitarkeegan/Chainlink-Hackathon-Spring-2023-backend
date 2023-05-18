ea3bb1fb4df4a5c3642a6ab73e017f0eca2b97a3
guitarkeegan <keegananglim@gmail.com>
2023-05-18 14:15:25 -0700

    add pokerchips token

---
+++ contracts/BetToken.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract PokerChips is ERC20, Ownable {
+    constructor() ERC20("PokerChips", "PC") {}
+    // initial supply amount 1 billion would be 1000000000e18 
+    constructor(uint256 initialSupply) ERC20("PokerChips", "PC");
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+}
