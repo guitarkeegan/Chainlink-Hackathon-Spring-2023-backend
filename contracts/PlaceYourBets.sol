3e1d3284ecdc6382aed74da9dcf73a30fa4b7818
guitarkeegan <keegananglim@gmail.com>
2023-05-21 11:57:32 -0700

    add choice 1 and 2 arrays to betpool struct

--- contracts/PlaceYourBets.sol
+++ contracts/PlaceYourBets.sol
@@ -2,8 +2,8 @@
 pragma solidity ^0.8.7;
 
 contract PlaceYourBets {
-
     struct BetPool {
+        address creator;
         string title;
         string description;
         string choice1;
@@ -11,10 +11,11 @@ contract PlaceYourBets {
         BetStatus status;
         // assume even 1:1 odds for now
         // have a set bet amount for now
-        uint256 bet_amount;
-        uint256 total_pot;
-        uint8 winning_option; // 1 or 2
-        mapping(address => Bet) bets; // causeing error
+        uint256 betAmount;
+        uint256 totalPot;
+        uint8 winningOption; // 1 or 2
+        address[] choice1Bets;
+        address[] choice2Bets;
     }
 
     struct Bet {
@@ -25,7 +26,7 @@ contract PlaceYourBets {
 
     // errors
     error BET_AMOUNT_MUST_BE_GREATER_THAN_ZERO();
-
+    // store all pools
     mapping(uint256 => BetPool) public pools;
     uint256 pool_count;
 
@@ -35,22 +36,26 @@ contract PlaceYourBets {
         COMPLETED
     }
 
-    function createPool(string memory title, string memory description, string memory choice1, string memory choice2, uint256 bet_amount) public {
-        // check 
-        uint256 fixed_bet_amount;
-        if (bet_amount > 0){
-            fixed_bet_amount = bet_amount;
-        } else {
+    function createBetPool(
+        string memory _title,
+        string memory _description,
+        string memory _choice1,
+        string memory _choice2,
+        uint256 _betAmount
+    ) public returns (uint256) {
+        if (_betAmount <= 0) {
             revert BET_AMOUNT_MUST_BE_GREATER_THAN_ZERO();
         }
-        // create a new pool and add it to the pools
-        
-        // require whitelisted accounts?
-        
+        BetPool memory newPool = BetPool(msg.sender, _title, _description, _choice1, _choice2, BetStatus.OPEN, _betAmount, 0, 0, new address[](0), new address[](0));
+
+        pools[pool_count] = newPool;
+        pool_count++;
+
+        return pool_count - 1;
     }
 
     // function createBet(){}
-    
+
     // function availableBets(){}
     // function getBetDetails(){}
     // function getBetStatus(){}
