1a370c83ef1330808a4d26d33925df56b2f4b049
guitarkeegan <keegananglim@gmail.com>
2023-06-06 23:48:09 -0700

    before refactor

--- contracts/PlaceYourBets.sol
+++ contracts/PlaceYourBets.sol
@@ -6,8 +6,7 @@ import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "@chainlink/contracts/src/v0.8/interfaces/AutomationCompatibleInterface.sol";
 import "@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract PlaceYourBets is ChainlinkClient, AutomationCompatibleInterface, ConfirmedOwner {
-    using Chainlink for Chainlink.Request;
+contract PlaceYourBets is AutomationCompatibleInterface, ConfirmedOwner {
     
     struct BetPool {
         address creator;
@@ -55,18 +54,8 @@ contract PlaceYourBets is ChainlinkClient, AutomationCompatibleInterface, Confir
     bytes32 public rank;
     bytes32 public tier;
     
-    event RequestMultipleFullfilled(
-        bytes32 indexed requestId,
-        bytes32 rank,
-        bytes32 tier
-    );
-
     constructor() ConfirmedOwner(msg.sender){
         i_owner = msg.sender;
-        setChainlinkToken(0x326C977E6efc84E512bB9C30f76E30c160eD06FB);
-        setChainlinkOracle(0x40193c8518BB267228Fc409a613bDbD8eC5a97b3);
-        jobId = "7d80a6386ef543a3abb52817f6707e3b";
-        fee = (1 * LINK_DIVISIBILITY) / 10; // 0,1 * 10**18 (Varies by network and job)
     }
 
     function performUpkeep(bytes calldata /* performData */) external override{
@@ -87,58 +76,6 @@ contract PlaceYourBets is ChainlinkClient, AutomationCompatibleInterface, Confir
             }
         }
         upkeepNeeded = result;
-    }
-
-    function requestFirstId() public returns (bytes32 requestId) {
-        Chainlink.Request memory req = buildChainlinkRequest(
-            jobId,
-            address(this),
-            this.fulfill.selector
-        );
-
-        // Set the URL to perform the GET request on
-        // riotgames api
-        // dev key
-        req.add(
-            "get",
-            "https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/1Y4w2fpEzxMONISK2M3MNPudYdDgd_bAtq-g0EE-KZ76qsE?api_key=RGAPI-1ad43d58-3ddb-4396-909e-8d4183224e4f"
-        );
-
-        // Set the path to find the desired data in the API response, where the response format is:
-        // [{
-        //  "id": "bitcoin",
-        //  "symbol": btc",
-        // ...
-        // },
-        //{
-        // ...
-        // .. }]
-        // request.add("path", "0.id"); // Chainlink nodes prior to 1.0.0 support this format
-        req.add("path", "0,rank"); // Chainlink nodes 1.0.0 and later support this format
-        // Sends the request
-        return sendChainlinkRequest(req, fee);
-    }
-
-    /**
-     * Receive the response in the form of string
-     */
-    function fulfill(
-        bytes32 _requestId,
-        string memory _id
-    ) public recordChainlinkFulfillment(_requestId) {
-        emit RequestFirstId(_requestId, _id);
-        id = _id;
-    } 
-
-     /**
-     * Allow withdraw of Link tokens from the contract
-     */
-    function withdrawLink() public onlyOwner {
-        LinkTokenInterface link = LinkTokenInterface(chainlinkTokenAddress());
-        require(
-            link.transfer(msg.sender, link.balanceOf(address(this))),
-            "Unable to transfer"
-        );
     }
 
     function createBetPool(
